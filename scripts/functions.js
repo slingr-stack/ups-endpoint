7fd030af6f45917dd6fdc34d8d7023c3a675bfde
Hugo Pacini <hugodpn@gmail.com>
2019-10-03 12:53:36 -0300

    #9861 Add security object and js helpers.

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,5 +1,37 @@
-endpoint.testing = function () {
+endpoint.track = {};
+endpoint.track.request = function (reqObj) {
+    return endpoint.post('/Track', reqObj);
+};
 
-    sys.logs.info("Testing UPS endpoint");
-    return {status: "ok"};
+endpoint.track.trackByInquiryNumber = function (inquiryNumber) {
+
+    var reqObj = {
+        "TrackRequest": {
+            "Request": {
+                "RequestOption": "1",
+                "TransactionReference": {
+                    "CustomerContext": new Date().toString()
+                }
+            },
+            "InquiryNumber": inquiryNumber
+        }
+    };
+
+
+    return endpoint.post('/Track', reqObj);
+};
+
+endpoint.request = function (url, reqObj) {
+    return endpoint.post(url, reqObj);
+};
+
+/////////////////////////////////////
+// Public API - Generic Functions
+/////////////////////////////////////
+
+endpoint.post = function (url, options) {
+    return endpoint._post({
+        path: url,
+        body: options
+    });
 };
--- src/main/java/io/slingr/endpoints/ups/UPSEndpoint.java
+++ src/main/java/io/slingr/endpoints/ups/UPSEndpoint.java
@@ -1,14 +1,21 @@
 package io.slingr.endpoints.ups;
 
-import io.slingr.endpoints.Endpoint;
+import io.slingr.endpoints.HttpEndpoint;
+import io.slingr.endpoints.framework.annotations.EndpointFunction;
 import io.slingr.endpoints.framework.annotations.EndpointProperty;
 import io.slingr.endpoints.framework.annotations.SlingrEndpoint;
+import io.slingr.endpoints.utils.Json;
+import io.slingr.endpoints.ws.exchange.FunctionRequest;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
-@SlingrEndpoint(name = "fedex")
-public class UPSEndpoint extends Endpoint {
+@SlingrEndpoint(name = "ups", functionPrefix = "_")
+public class UPSEndpoint extends HttpEndpoint {
 
     private static final Logger logger = Logger.getLogger(UPSEndpoint.class);
+
+    private static final String PRDUCTION_URL = "https://onlinetools.ups.com/rest";
+    private static final String TESTING_URL = "https://wwwcie.ups.com/rest";
 
     @EndpointProperty
     private String environment;
@@ -22,6 +29,55 @@ public class UPSEndpoint extends Endpoint {
     @EndpointProperty
     private String apiKey;
 
+    @Override
+    public String getApiUri() {
+        return StringUtils.equals("PRODUCTION", environment) ? PRDUCTION_URL : TESTING_URL;
+    }
 
+    @Override
+    public void endpointStarted() {
+        this.httpService().setDebug(true);
+        this.httpService().setDefaultEmptyPath("");
+        this.httpService().setupDefaultHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        this.httpService().setupDefaultHeader("Access-Control-Allow-Methods", "POST");
+        this.httpService().setupDefaultHeader("Access-Control-Allow-Origin", "*");
+        this.httpService().setupDefaultHeader("Accept", "application/json");
+        this.httpService().setupDefaultHeader("Content-Type", "application/json");
+    }
 
+    @EndpointFunction(name = "_post")
+    public Json postRequest(FunctionRequest request) {
+
+        Json req = request.getJsonParams();
+        String path = req.string("path");
+        logger.debug(String.format("[POST] request to %s", path));
+
+        Json body = req.json("body");
+        if (body == null) {
+            body = Json.map();
+        }
+
+        setUpsSecurity(body);
+
+        req.set("body", body);
+        return httpService().defaultPostRequest(req);
+
+    }
+
+    private void setUpsSecurity(Json body) {
+
+        Json userToken = Json.map()
+                .set("UsernameToken", Json.map()
+                        .set("Username", userName)
+                        .set("Password", password)
+                )
+                .set("ServiceAccessToken", Json.map()
+                        .set("AccessLicenseNumber", apiKey)
+                );
+
+        if (body != null) {
+            body.set("UPSSecurity", userToken);
+        }
+
+    }
 }
