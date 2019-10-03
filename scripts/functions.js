b8f6828578e032c435bba5543308030664c0ad69
Hugo Pacini <hugodpn@gmail.com>
2019-10-03 15:01:33 -0300

    #9861 Add helpers.

--- scripts/functions.js
+++ scripts/functions.js
@@ -3,6 +3,41 @@ endpoint.track.post = function (reqObj) {
     return endpoint.post('/Track', reqObj);
 };
 
+endpoint.pickUp = {};
+endpoint.pickUp.post = function (reqObj) {
+    return endpoint.post('/Pickup', reqObj);
+};
+
+endpoint.rate = {};
+endpoint.rate.post = function (reqObj) {
+    return endpoint.post('/Rate', reqObj);
+};
+
+endpoint.ship = {};
+endpoint.ship.post = function (reqObj) {
+    return endpoint.post('/Ship', reqObj);
+};
+
+endpoint.void = {};
+endpoint.void.post = function (reqObj) {
+    return endpoint.post('/Void', reqObj);
+};
+
+endpoint.lbRecovery = {};
+endpoint.lbRecovery.post = function (reqObj) {
+    return endpoint.post('/LBRecovery', reqObj);
+};
+
+endpoint.timeInTransit = {};
+endpoint.timeInTransit.post = function (reqObj) {
+    return endpoint.post('/TimeInTransit', reqObj);
+};
+
+endpoint.av = {};
+endpoint.av.post = function (reqObj) {
+    return endpoint.post('/AV', reqObj);
+};
+
 endpoint.track.trackByInquiryNumber = function (inquiryNumber) {
 
     var reqObj = {
